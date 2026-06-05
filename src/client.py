11e924b0a35fc912105300418fe32d9e7aea6f8e
danielelk <danielkhouryprogramming@gmail.com>
2026-06-05 21:52:30 +0200

    refactor: call client inside get_book_text instead of passing it as an argument

--- src/client.py
+++ src/client.py
@@ -7,18 +7,13 @@ def create_client():
     return Client(base_url=BASE_URL)
 
 
-def get_book_text(client: Client, book_id: str):
-    response = client.get(f"cache/epub/{book_id}/pg{book_id}.txt")
+def get_book_text(book_id: str):
+    with create_client() as client:
+        response = client.get(f"cache/epub/{book_id}/pg{book_id}.txt")
     if response.status_code == 404:
         raise Exception(f"No book with the id {book_id}")
     response.raise_for_status()
     return response.text
 
 
-def main():
-    with create_client() as client:
-        print(get_book_text(client, 45698453612))
 
-
-if __name__ == "__main__":
-    main()
--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 import argparse
+from src.client import get_book_text
 
 # Initialize parser
 parse = argparse.ArgumentParser()
 
 # Add argumnets
 parse.add_argument("--lexdiv", help=f"Get metrics about the book's vocabulary")
+
+arguments = parse.parse_args()
+
+book_id = arguments.lexdiv
+
+print(get_book_text(book_id))
+
